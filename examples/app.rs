e633d1db229d7ed0e5cff91eaa09eeecd2d5b914
Matt Hunzinger <matthunz2@gmail.com>
2023-10-15 12:11:19 -0400

    Add duration argument

--- examples/app.rs
+++ examples/app.rs
@@ -1,17 +1,17 @@
 use dioxus::prelude::*;
 use dioxus_spring::use_spring;
+use std::time::Duration;
 
 fn app(cx: Scope) -> Element {
-    let spring_ref = use_spring(cx, 10, 100, |font_size| {
+    let spring_ref = use_spring(cx, 10f32, 100f32, Duration::from_secs(1), |font_size| {
         format!("font-size: {font_size}px;")
     });
 
-    render!(h1 {
-        onmounted: move |event| {
-            spring_ref.mount(event.data);
-        },
-        "Hello World!"
-    })
+    render!(
+        h1 { onmounted: move |event| {
+                spring_ref.mount(event.data);
+            }, "Hello World!" }
+    )
 }
 
 fn main() {
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,16 @@ use futures::StreamExt;
 use futures_channel::mpsc;
 use interpolation::Lerp;
 use js_sys::Date;
-use std::{fmt, rc::Rc};
+use std::{fmt, rc::Rc, time::Duration};
 use wasm_bindgen::{prelude::Closure, JsCast};
 
-pub fn use_spring<T, V>(cx: Scope<T>, from: V, to: V, mut make_style: impl FnMut(V) -> String + 'static) -> SpringRef
+pub fn use_spring<T, V>(
+    cx: Scope<T>,
+    from: V,
+    to: V,
+    duration: Duration,
+    mut make_style: impl FnMut(V) -> String + 'static,
+) -> SpringRef
 where
     V: Lerp<Scalar = f32> + fmt::Display + 'static,
 {
@@ -15,7 +21,12 @@ where
 
     let element_ref_clone = element_ref.clone();
     cx.spawn(async move {
-        for _ in 0..100 {
+        loop {
+            let dt = Date::now() - start;
+            if dt >= duration.as_secs_f64() * 1000. {
+                break;
+            }
+
             if let Some(element) = &*element_ref_clone.read() {
                 let raw_elem = element
                     .get_raw_element()
@@ -23,12 +34,9 @@ where
                     .downcast_ref::<web_sys::Element>()
                     .unwrap();
 
-                let dt = Date::now() - start;
-                let v = interpolation::lerp(&from, &to, &((dt / 1000.) as f32));
-
-                raw_elem
-                    .set_attribute("style", &make_style(v))
-                    .unwrap();
+                let x = dt / (duration.as_secs_f64() * 1000.);
+                let v = interpolation::lerp(&from, &to, &(x as f32));
+                raw_elem.set_attribute("style", &make_style(v)).unwrap();
             }
 
             let (tx, mut rx) = mpsc::unbounded();
